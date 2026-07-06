b481d4a78718a0c3dc970ed587af0e7d70ac43a9
William (Lindy) Lindstrom <llindstrom@lco.global>
2026-07-06 15:27:30 -0700

    update the KeckProfile to use EncryptedModelField

--- tom_keck/models.py
+++ tom_keck/models.py
@@ -1,19 +1,23 @@
 import logging
 
+from django.contrib.auth.models import User
 from django.db import models
 
-from tom_common.models import EncryptableModelMixin, EncryptedProperty
+from tom_common.encryption import EncryptedModelField
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class KeckProfile(EncryptableModelMixin, models.Model):
+class KeckProfile(models.Model):
     """User Profile for the TOMToolkit Keck Facility
     """
-    keck_username = models.CharField(
-        max_length=255, null=True, blank=True, verbose_name="Swift Username"
-    )
+    # connect this Profile to it's User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    _keck_password = models.BinaryField(null=True, blank=True)
-    keck_password = EncryptedProperty("_keck_password")
+    keck_username = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Keck Username"
+    )
+    keck_password = EncryptedModelField(null=True, blank=True)
+
